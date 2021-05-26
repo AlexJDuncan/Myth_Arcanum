a1f7eb972b1fa495f970361933755a7d21683914
alex <monkeymedic26@gmail.com>
2021-05-25 22:18:14 -0700

    set style for topbar and add title

--- client/src/TopBar.jsx
+++ client/src/TopBar.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const TopBarStyle = styled.div`
+  height: 125px;
+  background: black;
+  color: white;
+`;
 
 const TopBar = () => {
     return (
-        <div>Hello World</div>
+        <TopBarStyle>
+          <span>        
+            <h1 style={{margin: 0 + 'px'}}>Myth Arcanum</h1>    
+          </span>
+        </ TopBarStyle>
     )
 }
 
